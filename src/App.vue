17d4d0d851edfe59667e18c2b0211c1bdad1f67b
Alexander Six <alexanderhsix@gmail.com>
2018-03-12 16:59:57 -0400

    Adds some explanatory comments about Vue SPA structure and updates the eslint strength

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
+    <!-- This is where things that belong on every page go (navbar, logo, etc) -->
     <img src="./assets/logo.png">
+
+    <!-- Router-view is where the components will get swapped in when
+    we navigate around the application -->
     <router-view/>
   </div>
 </template>
