7ac876f98c7d66746b0f7124265d3a8cd3ca4ce6
Alexander Six <alexanderhsix@gmail.com>
2018-03-13 16:32:33 -0400

    Adds good, average, and bad day components and assigns them to specific routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import GoodDay from '@/components/GoodDay'
+import AverageDay from '@/components/AverageDay'
+import BadDay from '@/components/BadDay'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+    { path: '/', name: 'HelloWorld', component: HelloWorld },
+    { path: '/good-day', name: 'GoodDay', component: GoodDay },
+    { path: '/average-day', name: 'AverageDay', component: AverageDay },
+    { path: '/bad-day', name: 'BadDay', component: BadDay }
   ]
 })
