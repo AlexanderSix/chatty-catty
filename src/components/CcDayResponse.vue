4f982d055e1bc5d14f6ff7eea8e4ae14ac70210b
Alexander Six <alexanderhsix@gmail.com>
2018-03-14 15:53:00 -0400

    Adds the CcDayResponse component and scaffolds the storing method

---
+++ src/components/CcDayResponse.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <form @submit.prevent="submitResponse()">
+      <textarea
+        id="response"
+        v-model="response.text"
+      />
+      <button
+        type="Submit"
+        name="Submit"
+      >
+        Submit
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CcDayResponse',
+
+  data () {
+    return {
+      response: {
+        text: ''
+      }
+    }
+  },
+
+  methods: {
+    submitResponse () {
+      // TODO: store into a DB using a backend API
+      // For now: spoof the storage of the text
+      console.log('Saving: ' + this.response.text)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
