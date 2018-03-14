f72c67d403d730d94e9e913570c93ceb4286eb95
Alexander Six <alexanderhsix@gmail.com>
2018-03-14 09:00:03 -0400

    Adds a template vue file for the face buttons

---
+++ src/components/CcFaceButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <router-link :to="linkDestination">{{ linkText }}</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CcFaceButton',
+  props: {
+    imgName: {
+      type: String,
+      required: true
+    },
+    linkText: {
+      type: String,
+      required: true
+    },
+    linkDestination: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
