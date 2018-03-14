488cc500256133e490db5165afeb480e253c653b
Alexander Six <alexanderhsix@gmail.com>
2018-03-14 09:01:15 -0400

    Adjusts the dashboard template to use the face button templates

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,94 +1,44 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div>
+    <!-- This is the main page of the application shown in the mockups -->
+    <h3>How was your day today?</h3>
+
+    <div class="face-buttons">
+      <cc-face-button
+        img-name="good.png"
+        link-text="Good!"
+        link-destination="good-day"
+      />
+      <cc-face-button
+        img-name="avg.png"
+        link-text="Average."
+        link-destination="average-day"
+      />
+      <cc-face-button
+        img-name="bad.png"
+        link-text="Bad..."
+        link-destination="bad-day"
+      />
+    </div>
+
+    <div class="chatty-cat">
+      CAT GOES HERE
+    </div>
+
   </div>
 </template>
 
 <script>
+import CcFaceButton from '@/components/CcFaceButton'
+
 export default {
-  name: 'HelloWorld',
+  name: 'Dashboard',
+  components: {
+    CcFaceButton
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+
     }
   }
 }
