364c66f3681a91e3ced1ed083d5ad8ebf1653f1a
Alexander Six <alexanderhsix@gmail.com>
2018-03-27 16:16:55 -0400

    Adds an array for the user's cats (now in a class) and sets up some construction logic for the component

--- src/components/CatSelector.vue
+++ src/components/CatSelector.vue
@@ -1,22 +1,58 @@
 <template>
-  <div>
-    <cat type="add"/>
-    <cat type="myCat"/>
-    <cat type="friendCat"/>
+  <div id="cat-selector">
+    <cat
+      v-for="cat in catsArr"
+      :key="cat.id"
+      :type="cat.type"
+    />
   </div>
 </template>
 
 <script>
 import Cat from '@/components/Cat'
 
+// TODO: Implement switching focus on cats for mockup on Friday
+
+// Component-scope global to keep track of
+// number of cats
+var numberOfCats = 0
+
+class CatClass {
+  constructor (type) {
+    this.id = numberOfCats++
+    this.type = type
+  }
+}
+
 export default {
   name: "CatSelector",
+
+  /**
+   * Basically acts as a constructor--this method is
+   * called when the component is created
+   */
+  created () {
+    this.setUpCatsArr()
+  },
+
   components: {
     Cat
   },
+
   data () {
     return {
+      catsArr: []
+    }
+  },
 
+  methods: {
+    /**
+     * Sets up the cats array to always have the add cat
+     * as the first cat, and the user's cat as the second
+     */
+    setUpCatsArr () {
+      this.catsArr[0] = new CatClass('add')
+      this.catsArr[1] = new CatClass('myCat')
     }
   }
 }
