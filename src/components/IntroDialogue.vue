6acdbc0e097d5d0b16852068fa1b888c1b66caf0
Luke Wilson <lucaswilson@mac.com>
2018-03-28 21:10:15 -0400

    filled in the intro and my cat dialogues

--- src/components/IntroDialogue.vue
+++ src/components/IntroDialogue.vue
@@ -1,15 +1,23 @@
 <template>
-
+  <div>
+    <h3>Welcome to Chatty Catty!</h3>
+    <p>I'm your new virtual cat! Please provide your phone number so that your friends can find you!</p><br>
+    <input type="number" name="pnumber">
+    <button>Submit</button>
+  </div>
 </template>
 
 <script>
 export default {
+  name: "IntroDialogue",
+  data (){
+    return{
 
+    }
+  }
 }
 </script>
 
 <style>
 
 </style>
-
-
--- src/components/MyCat.vue
+++ src/components/MyCat.vue
@@ -1,10 +1,38 @@
 <template>
-
+  <div>
+    <h3>How was your day today?</h3>
+    <div class="face-buttons">
+  <cc-face-button
+    img-name="good.png"
+    link-text="Good!"
+    link-destination="good-day"
+  />
+  <cc-face-button
+    img-name="avg.png"
+    link-text="Average."
+    link-destination="average-day"
+  />
+  <cc-face-button
+    img-name="bad.png"
+    link-text="Bad..."
+    link-destination="bad-day"
+  />
+</div>
+  </div>
 </template>
 
 <script>
+import CcFaceButton from '@/components/CcFaceButton'
 export default {
+  name:"MyCat",
+  components: {
+    CcFaceButton
+  },
+  data(){
+    return{
 
+    }
+  }
 }
 </script>
 
