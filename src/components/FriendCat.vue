fa5dbdfb914a97bcf93ca652223ff2ca115cf413
Alexander Six <alexanderhsix@gmail.com>
2018-03-29 00:01:06 -0400

    Added basic Friend checkup template (will add chart later)

--- src/components/FriendCat.vue
+++ src/components/FriendCat.vue
@@ -1,10 +1,18 @@
 <template>
-
+  <div>
+    <h3>{{ header }}</h3>
+    <p>Chart will go here</p>
+  </div>
 </template>
 
 <script>
 export default {
-
+  name: "FriendCat",
+  data () {
+    return {
+      header: "How's Your Friend?" // TODO: Make sure there is a name associated in the DB (when we build this for real)
+    }
+  }
 }
 </script>
 
