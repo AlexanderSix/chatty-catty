aeebd414932e9d94906cb5bc6e605361e093a4d6
Alexander Six <alexanderhsix@gmail.com>
2018-03-28 22:45:06 -0400

    Initial AddCat component flushed out

--- src/components/AddCat.vue
+++ src/components/AddCat.vue
@@ -1,10 +1,30 @@
 <template>
-
+  <div>
+    <h3>{{ header }}</h3>
+    <p>{{ text }}</p>
+    <input type="number" name="pnumber" v-model="myNumber">
+    <button @click="submitFriendPhoneNumber()">Submit</button>
+  </div>
 </template>
 
 <script>
 export default {
-
+  name: "AddCat",
+  data () {
+    return {
+      header: "Add a Friend!",
+      text: "Enter your friend's phone number below to add them to your list (we'll change this text)",
+      myNumber: ""
+    }
+  },
+  methods: {
+    submitFriendPhoneNumber () {
+      this.myNumber = ""
+      this.$parent.$emit('endintro')
+      this.header = "Your friend has been added!"
+      this.text = `"Your friend's cat has been added! You can now check in on them by clicking on their cat along the bottom of your screen!" <br> Have you checked in yet today? Click on your cat to do a quick check-in and let it know how you're doing!`
+    }
+  }
 }
 </script>
 
--- src/components/IntroDialogue.vue
+++ src/components/IntroDialogue.vue
@@ -2,7 +2,7 @@
   <div>
     <h3>{{ header }}</h3>
     <p>{{ text }}</p><br>
-    <input type="number" name="pnumber">
+    <input type="number" name="pnumber" v-model="myNumber">
     <button @click="submitPhoneNumber()">Submit</button>
   </div>
 </template>
@@ -13,14 +13,16 @@ export default {
   data () {
     return{
       header: "Welcome to Chatty Catty!",
-      text: "I'm your new virtual cat! Please provide your phone number so that your friends can find you!"
+      text: "I'm your new virtual cat! Please provide your phone number so that your friends can find you!",
+      myNumber: ""
     }
   },
   methods: {
     submitPhoneNumber () {
+      this.myNumber = ""
       this.$parent.$emit('addintro')
       this.header = "Add a friend!"
-      this.text = "I can only do my job if you add a friend's cat for me to chat with! Click on the cat to my left to start adding friends!"
+      this.text = "I can only do my job if you add a friend's cat for me to chat with! Click on the cat to my left to start adding friends and watch as they appear along the bottom of your screen!"
     }
   }
 }
