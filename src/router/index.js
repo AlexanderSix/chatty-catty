d9dcd725d196ff12fd26b0d3efc3a8fcfa50e715
Alexander Six <alexanderhsix@gmail.com>
2018-04-15 11:52:15 -0400

    Makes adjustments to the Router so that the user is redirected to login if their login token is not stored in HTML5 localstorage

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,36 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import CatContainer from '@/components/CatContainer'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
-    { path: '/', name: 'CatContainer', component: CatContainer }
+    { path: '/', name: 'CatContainer', component: CatContainer },
+    { path: '/login', name: 'Login', component: Login }
   ]
 })
+
+/**
+ * Middleware that runs before each route
+ */
+router.beforeEach((to, from, next) => {
+  // Stores the login token in localstorage so even if the
+  // page is closed, the login will be valid (until
+  // logged out intentionally)
+  let loginToken = localStorage.getItem('logintoken')
+
+  // If the router is going to login, continue there
+  if (to.path === '/login') {
+    next()
+  }
+
+  if (loginToken === "" || loginToken === undefined || loginToken === null) {
+    next('/login')
+  } else {
+    next(to)
+  }
+})
+
+export default router
